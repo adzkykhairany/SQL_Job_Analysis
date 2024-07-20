f2bcd270b62f9c82976ff616ccabd21960e90fd3
adzkykhairany <adzkyykhairany@gmail.com>
2024-07-20 18:10:15 +0700

    top paying jobs

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,20 @@
+-- Top 10 highest-paying Data Analyst roles that are avalaible remotely.
+
+SELECT
+    job_id,
+    job_title,
+    name AS company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact a
+LEFT JOIN company_dim b ON a.company_id = b.company_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
