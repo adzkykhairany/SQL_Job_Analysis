cbbf83011087b6d5cc20390bcec2e2b188ffbf65
adzkykhairany <adzkyykhairany@gmail.com>
2024-07-20 20:13:33 +0700

    editing top demand

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,4 +1,4 @@
--- Most in-demand skills Data Analyst
+-- Most in-demand skills for Data Analyst Jobs
 
 SELECT 
     skills,
@@ -9,6 +9,10 @@ INNER JOIN
     skills_job_dim d ON c.job_id = d.job_id
 INNER JOIN 
     skills_dim e ON d.skill_id = e.skill_id 
+WHERE
+    job_title_short = 'Data Analyst'
 GROUP BY
     skills
+ORDER BY 
+    demand_count DESC
 LIMIT 5
